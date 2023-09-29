83fe8931e0624da3d119745e43a6f7e37aed7394
fyosetake <fyosetake@gmail.com>
2023-09-29 18:54:41 -0300

    Cria controllers indiviualizados para 'listarVendas' e 'cadastrarVenda'

---
+++ app/Http/Controllers/ListarVendasController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\ListarVendasService;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ListarVendasController extends Controller
+{
+    private $listarVendasService;
+
+    public function __construct(ListarVendasService $listarVendasService)
+    {
+        $this->listarVendasService = $listarVendasService;
+    }
+
+    public function listarVendas()
+    {
+        try {
+            $vendas = $this->listarVendasService->listarVendas();
+            return new Response($vendas, 200);
+        } catch (\Exception $e) {
+            return new Response(['error' => 'Ocorreu um erro ao listar as vendas: ' . $e->getMessage()], 500);
+        }
+    }
+
+    public function listarVendasVendedor($vendedor_id)
+    {
+        try {
+            $vendas = $this->listarVendasService->listarVendas($vendedor_id);
+            return new Response($vendas, 200);
+        } catch (\Exception $e) {
+            return new Response(['error' => 'Ocorreu um erro ao listar as vendas: ' . $e->getMessage()], 500);
+        }
+    }
+}

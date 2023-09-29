e13197318effe7e13884220abe1c387088cd59a0
fyosetake <fyosetake@gmail.com>
2023-09-29 16:24:02 -0300

    Cria o método 'deletarVendedor' no repository 'VendedoresRepository'

--- app/Repositories/VendedoresRepository.php
+++ app/Repositories/VendedoresRepository.php
@@ -15,4 +15,18 @@ class VendedoresRepository
     {
         return Vendedor::all();
     }
+
+    public function deletarVendedor($vendedor_id)
+    {
+        $vendedor = Vendedor::find($vendedor_id);
+
+        if ($vendedor) {
+            $vendedor->vendas()->delete();
+            $vendedor->delete();
+
+            return true;
+        }
+
+        return false;
+    }
 }
